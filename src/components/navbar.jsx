2c8a8bbcd2b86e0899568eb3bd25944559116c3f
Muhammad Najeeb Yusuf <89583643+Yusuf-Najeeb@users.noreply.github.com>
2025-02-26 08:01:31 +0100

    Fixed: Blank screen issue (#2)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import { NavLink } from 'react-router'
+import React from "react";
+import { NavLink } from "react-router";
 
 const navbar = () => {
   return (
     <div>
-        <nav className='navbar'>
-    <div className="navbar-brand">
-      <p>DREAMRIDE</p>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <p>DREAMRIDE</p>
+        </div>
+        <ul className="navbar-menu">
+          <div className="navbar-middle-content">
+            <li>
+              <a href="#home" className="navbar-link active">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#fleet" className="navbar-link active">
+                Cars
+              </a>
+            </li>
+            {/* <li><a href="#Sign In" className="navbar-link active">Sign In</a></li> */}
+            <li>
+              <a href="#features" className="navbar-link active">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="navbar-link active">
+                Contact
+              </a>
+            </li>
+          </div>
+          <div className="profile">
+            <button className="signin-button">
+              <a href="#fleet">Rent a Car</a>
+            </button>
+          </div>
+        </ul>
+      </nav>
     </div>
-    <ul className="navbar-menu">
-     <div className='navbar-middle-content'>
-     <li><a href="#home" className="navbar-link active">Home</a></li>
-      <li><a href="#cars" className="navbar-link active">Cars</a></li>
-      {/* <li><a href="#Sign In" className="navbar-link active">Sign In</a></li> */}
-      <li><a href="#Features" className="navbar-link active">Features</a></li>
-      <li><a href="#Contact" className="navbar-link active">Rent a Car</a></li>
-     </div>
-      <div className='profile'>
-      <button className="signin-button">Sign In</button>
-      </div>
-    </ul>
-  </nav>
-    </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default navbar;
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import ServiceImage from "../assets/images/service-section-image.jpg"
+import React from "react";
+import ServiceImage from "../assets/images/service-section-image.jpg";
 
 const services = () => {
   return (
     <div>
-    <section className="customer-service">
-      <div className="image-container">
-        <img src={ServiceImage} alt="Luxury Car" />
-      </div>
-      <div className="text-container">
-        <h2>Superior <br /> Customer Service</h2>
-        <p>
-          Experience exotic car rentals from the brand known for award-winning customer service.
-          Our goal is to meet your needs and exceed your expectations. We'll even deliver and collect
-          your exotic vehicle at your convenience.
-        </p>
-        <button className="rent-a-car">Rent a Car</button>
-      </div>
-    </section>
+      <section className="customer-service" id="services">
+        <div className="image-container">
+          <img src={ServiceImage} alt="Luxury Car" />
+        </div>
+        <div className="text-container">
+          <h2>
+            Superior <br /> Customer Service
+          </h2>
+          <p>
+            Experience exotic car rentals from the brand known for award-winning
+            customer service. Our goal is to meet your needs and exceed your
+            expectations. We'll even deliver and collect your exotic vehicle at
+            your convenience.
+          </p>
+          <button className="rent-a-car">Rent a Car</button>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default services
+export default services;
